9d65d454b49e4ad034426ca1d4db3f76b4c44754
mihaibaluta-prog <mihaibaluta65@gmail.com>
2026-06-17 22:45:49 +0200

    Update App.tsx

--- artifacts/da-francesco/src/App.tsx
+++ artifacts/da-francesco/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { I18nProvider } from "@/lib/i18n";
 import Home from "@/pages/Home";
 import Datenschutz from "@/pages/Datenschutz";
+import Impressum from "@/pages/Impressum";
 
 function useHash() {
   const [hash, setHash] = useState(() => window.location.hash.replace("#", ""));
@@ -17,7 +18,13 @@ function App() {
   const hash = useHash();
   return (
     <I18nProvider>
-      {hash === "datenschutz" ? <Datenschutz /> : <Home />}
+      {hash === "datenschutz" ? (
+  <Datenschutz />
+) : hash === "impressum" ? (
+  <Impressum />
+) : (
+  <Home />
+)}
     </I18nProvider>
   );
 }
